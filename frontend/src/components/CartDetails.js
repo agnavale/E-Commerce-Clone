5f6672c7ea8877451903c359693ee52c3a18f6ad
Atharv Navale <109289472+agnavale@users.noreply.github.com>
2025-03-13 15:37:45 +0530

    Update CartDetails.js

--- frontend/src/components/CartDetails.js
+++ frontend/src/components/CartDetails.js
@@ -1,10 +1,19 @@
+
 const CartDetails = ({item}) => {
+    const handleClick = () => {
+        
+    }
     return ( 
         <div className="cart-details">
-            <h4> {item.name} </h4>
-            <p> price: {item.price} </p>
-            <p> qty: {item.qty} </p>
-            <button></button>
+            <div> 
+                <img src= "images/product.avif" />
+            </div>
+            <div>
+                <h4> {item.name} </h4>
+                <p> price: {item.price} </p>
+                <p> qty: {item.qty} </p>
+                <button onClick={ handleClick}> delete </button>
+            </div>
         </div>
     );
 }
