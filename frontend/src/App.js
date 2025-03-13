4cd479d16cc179945e91ad4384b72b032d17d459
Atharv Navale <109289472+agnavale@users.noreply.github.com>
2025-03-13 15:49:21 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,14 +19,8 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path="/" element = { <Home /> } /> 
-          </Routes>
-          <Routes>
             <Route path="/cart" element = { <Cart /> } /> 
-          </Routes>
-          <Routes>
             <Route path="/login" element = {!user ? <Login />: <Navigate to="/" />} /> 
-          </Routes>
-          <Routes>
             <Route path="/signup" element = {!user ? <Signup />:<Navigate to="/" />} /> 
           </Routes>
         </div>
