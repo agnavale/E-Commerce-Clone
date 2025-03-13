ece85ca49b26dd7949d59132732dcdfa6d88ced1
Atharv Navale <109289472+agnavale@users.noreply.github.com>
2025-03-13 10:59:52 +0530

    Update useLogin.js

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -10,7 +10,9 @@ export const useLogin = () => {
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch('/api/user/login', {
+        const BASE_URL = process.env.REACT_APP_BACKEND_URI;
+        
+        const response = await fetch(`${BASE_URL}/api/user/login`, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({email, password})
