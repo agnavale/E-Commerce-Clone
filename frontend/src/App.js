59a2c6881a6d6a6b20fb371630dd5da329f24984
Atharv Navale <109289472+agnavale@users.noreply.github.com>
2025-03-14 22:10:09 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,9 +7,6 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Cart from './pages/Cart';
 
-
-
-
 function App() {
   const { user } = useAuthContext()
   return (
@@ -19,13 +16,12 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path="/" element = { <Home /> } /> 
-            <Route path="/cart" element = { <Cart /> } /> 
+            <Route path="/cart" element = {user ? <Cart />: <Navigate to="/login" /> } /> 
             <Route path="/login" element = {!user ? <Login />: <Navigate to="/" />} /> 
             <Route path="/signup" element = {!user ? <Signup />:<Navigate to="/" />} /> 
           </Routes>
         </div>
       </BrowserRouter>
-      
     </div>
   );
 }
