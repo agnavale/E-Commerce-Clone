005142e220876e9203b1050ba2e30868d376d858
Atharv Navale <109289472+agnavale@users.noreply.github.com>
2025-03-14 21:55:32 +0530

    Update CartDetails.js

--- frontend/src/components/CartDetails.js
+++ frontend/src/components/CartDetails.js
@@ -1,18 +1,101 @@
+import { useState, useEffect } from "react"
+import { useAuthContext } from "../hooks/useAuthContext"
+import { useCartContext } from "../hooks/useCartContext"
 
 const CartDetails = ({item}) => {
-    const handleClick = () => {
-        
+    const { dispatch } = useCartContext()
+    const { user } = useAuthContext()
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
+
+    // check if enough stock
+    useEffect(() => {
+        if (item.qty > item.stock) {
+            setError("Not enough stock");
+        } else {
+            setError(""); 
+        }
+    }, [item.qty, item.stock]); 
+
+    const handleDelete = async () => {
+        if (!user) {
+            return
+        }
+        setLoading(true)
+        const response = await fetch('/api/cart/' + item.product_id, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': `Bearer ${user.token}`
+            }
+        })
+        const json = await response.json()
+        console.log(json)
+
+        if (response.ok){
+            dispatch({type:'DELETE_ITEM', payload: {_id: item._id}})
+        }
+        setLoading(false)
     }
+
+    const handleQtyChange = (val) => {
+        return async() => {
+            if (!user) {
+                return
+            }
+            if (item.qty + val === 0) {
+                await handleDelete();  
+                return;
+            }
+            if( item.qty + val > item.stock){
+                setError('not enough stock')
+                return
+            }
+
+            setLoading(true)
+            const response = await fetch('/api/cart/' + item.product_id, {
+                method: 'PATCH',
+                headers: {
+                    'Authorization': `Bearer ${user.token}`,
+                    'Content-Type':'application/json'
+                },
+                body: JSON.stringify({qty: item.qty + val})
+            })
+            const json = await response.json()
+    
+            if (response.ok){
+                dispatch({type:'UPDATE_ITEM', payload: json})
+            }
+            setLoading(false)
+        }
+    }
+
     return ( 
         <div className="cart-details">
             <div> 
-                <img src= "images/product.avif" />
+                <img src= "images/product.avif" alt="product-image" />
             </div>
-            <div>
+            <div className= {item.stock === 0 ? "strike": ""}>
                 <h4> {item.name} </h4>
-                <p> price: {item.price} </p>
-                <p> qty: {item.qty} </p>
-                <button onClick={ handleClick}> delete </button>
+                <p> <strong >Price:</strong>  {item.price} </p>
+                <p> <strong >Stock:</strong>  {item.stock} </p>
+
+                { item.stock === 0 ? (
+                    <div className="error"> Out of Stock </div>
+                ):(
+                     <div className="quantity">
+                        <button onClick={handleQtyChange(-1)} disabled={loading}>-</button>
+                        <p> {item.qty} </p>
+                        <button onClick={handleQtyChange(+1)}  disabled={loading}>+</button>
+                        {error && <div className="error"> {error} </div>}
+                    </div>
+                )}
+
+                <button className="material-symbols-outlined delete-btn" 
+                    onClick={handleDelete} 
+                    disabled={loading}
+                > 
+                    delete 
+                </button>
             </div>
         </div>
     );
